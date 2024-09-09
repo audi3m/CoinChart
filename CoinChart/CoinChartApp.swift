efb8b6f77e99bc1da3c5b5056a82e3c2f76b0c48
ray <audi3m@naver.com>
2024-09-09 14:54:51 +0900

    Initial Commit

---
+++ CoinChart/CoinChartApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinChartApp.swift
+//  CoinChart
+//
+//  Created by J Oh on 9/9/24.
+//
+
+import SwiftUI
+
+@main
+struct CoinChartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
